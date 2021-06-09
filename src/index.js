281a29dd425e3487b84e9f274f4094a6f126ce69
sandeep kumar <sandeepkss3335@gmail.com>
2021-06-09 19:00:13 +0530

    addding start package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const { join } = require('path');
 const app=express();
-const port=3000;
+const port=process.env.PORT || 3000;
 const path=require('path');
 const request=require('request');
 const path_static=path.join(__dirname,"../public");
