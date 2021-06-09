767dab7631667b69522975b161b0626dc2c33c83
sandeep kumar <sandeepkss3335@gmail.com>
2021-06-09 17:08:43 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+const path=require('path');
+const request=require('request');
+
+console.log(path.join(__dirname,"../public"))
+const staticpath=path.join(__dirname,"../public");
+app.use(express.static(staticpath));
+app.get("/", (req, res) =>{
+   res.send("sandy stark");
+});
+app.get("/about", (req,res) =>{
+   res.send("<html><h1>hello from the about section</h1></html>");
+});
+app.get("/temp", (req, res)=>{
+  request("http://api.openweathermap.org/data/2.5/weather?q=lakhimpur&appid=dfa855b8dd4ab21007d7e72a0ad49eef").on("data",(chunk)=>{
+     const objdata=JSON.parse(chunk);
+     const arrdata=[objdata];
+     console.log(arrdata[0].name);
+     res.send()
+  })
+  .on("end",(err)=>{
+     if(err){
+        return console.log("error",err);
+        res.end();
+     }
+  })
+})
+app.listen(3000, ()=>{
+   console.log("server online 3000");
+})
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const { join } = require('path');
+const app=express();
+const port=3000;
+const path=require('path');
+const request=require('request');
+const path_static=path.join(__dirname,"../public");
+const view_path=path.join(__dirname,"../views");
+app.use(express.static(path_static));
+app.set("views",view_path);
+
+app.get("/",(req, res) =>
+{
+   res.render('index.hbs');
+});
+app.get("/about",(req, res)=>{
+
+   res.render('about.hbs',{
+      susu : req.query.name
+   });
+})
+app.get("/temp", (req, res)=>{
+   request(`http://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=dfa855b8dd4ab21007d7e72a0ad49eef&units=metric`).on("data",(chunk)=>{
+      const objdata=JSON.parse(chunk);
+      const arrdata=[objdata];
+      console.log(req.query.name);
+      res.render("temp.hbs",{
+         city :arrdata[0].name,
+         temp: arrdata[0].main.temp
+      });
+   })
+   .on("end",(err)=>{
+      if(err){
+         return console.log("error",err);
+         res.end();
+      }
+   })
+ })
+
+app.listen(port,()=>
+{
+   console.log(`online server ${port} from index`);
+})
